84b472a7e628b5d6503535f017ea80ea3268e82b
Akring <ddflj3210@gmail.com>
2019-10-12 18:08:47 +0800

    More

--- Sources/iOS/OAuth2Authorizer+iOS.swift
+++ Sources/iOS/OAuth2Authorizer+iOS.swift
@@ -75,7 +75,7 @@ open class OAuth2Authorizer: OAuth2AuthorizerUI {
 	- parameter at:   The authorize URL to open
 	*/
 	public func authorizeEmbedded(with config: OAuth2AuthConfig, at url: URL) throws {
-		if #available(iOS 13, *), config.ui.useAuthenticationSession {
+		if #available(iOS 11, *), config.ui.useAuthenticationSession {
 			guard let redirect = oauth2.redirect else {
 				throw OAuth2Error.noRedirectURL
 			}
@@ -130,7 +130,7 @@ open class OAuth2Authorizer: OAuth2AuthorizerUI {
 	- parameter at:   The authorize URL to open
 	- returns:        A Boolean value indicating whether the web authentication session starts successfully.
 	*/
-	@available(iOS 13.0, *)
+	@available(iOS 11.0, *)
 	@discardableResult
 	public func authenticationSessionEmbedded(at url: URL, withRedirect redirect: String) -> Bool {
 		let completionHandler: (URL?, Error?) -> Void = { url, error in
@@ -146,9 +146,13 @@ open class OAuth2Authorizer: OAuth2AuthorizerUI {
 			}
 			self.authenticationSession = nil
 		}
-        
-		authenticationSession = ASWebAuthenticationSession(url: url, callbackURLScheme: redirect, completionHandler: completionHandler)
-		return (authenticationSession as! ASWebAuthenticationSession).start()
+		
+		if #available(iOS 13, *) {
+			authenticationSession = ASWebAuthenticationSession(url: url, callbackURLScheme: redirect, completionHandler: completionHandler)
+			return (authenticationSession as! ASWebAuthenticationSession).start()
+		} else {
+			return false;
+		}
 	}
 	
 	
